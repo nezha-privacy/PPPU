668cdfdf3c912774d0a8bc4081ca9d9b8304cb21
vctwwd <108380105+vctwwd@users.noreply.github.com>
2024-02-27 16:40:26 +0800

    Add files via upload

---
+++ src/example/benchmark/run_context_benchmark_2p.sh
@@ -0,0 +1,2 @@
+../../../build/BENCHMARK_CONTEXT pid=1 party=2 2>&1 >/dev/null &
+../../../build/BENCHMARK_CONTEXT pid=0 party=2
---
+++ src/example/benchmark/run_context_benchmark_3p.sh
@@ -0,0 +1,3 @@
+../../../build/BENCHMARK_CONTEXT pid=1 party=3 2>&1 >/dev/null &
+../../../build/BENCHMARK_CONTEXT pid=2 party=3 2>&1 >/dev/null &
+../../../build/BENCHMARK_CONTEXT pid=0 party=3
