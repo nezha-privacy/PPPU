ea99d37179cb22182ed39d265b5eec0abe4e47be
kk113 <144315109+kk141103@users.noreply.github.com>
2024-03-04 00:04:24 +0800

    Update tutorial.cpp

--- src/example/tutorials/tutorial.cpp
+++ src/example/tutorials/tutorial.cpp
@@ -4,6 +4,8 @@
 #include "tutorial_2_context.cpp"
 #include "tutorial_3_mpc.cpp"
 #include "tutorial_4_network.cpp"
+#include "tutorial_5_ndarray.cpp"
+#include "tutorial_6_visibility.cpp"
 
 int main(int argc, char* argv[]) {
     int pid = std::stoi(argv[1]);
@@ -16,5 +18,7 @@ int main(int argc, char* argv[]) {
     //tutorial::tutorial_2_context(pid, num_parties);
     //tutorial::tutorial_3_mpc(pid, num_parties);
     //tutorial::tutorial_4_network(pid, num_parties);
+    //tutorial::tutorial_5_ndarray();
+    //tutorial::tutorial_6_visibility(pid, num_parties);
     return 0;
 }
