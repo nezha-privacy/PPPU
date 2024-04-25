019c0a58cf74e1df51efc9ad7567bfc71ac3b2ab
vctwwd <108380105+vctwwd@users.noreply.github.com>
2024-04-25 14:51:58 +0800

    Update tutorial_5_ndarray.cpp

--- src/example/tutorials/tutorial_5_ndarray.cpp
+++ src/example/tutorials/tutorial_5_ndarray.cpp
@@ -34,7 +34,7 @@ void tutorial_5_ndarray() {
     std::vector<double> vec = {1.1, 2.2, 3.3, 4.4};
     core::NDArrayRef<double> ndarray = core::make_ndarray(vec);
     core::NDArrayRef<double> ndarray2 = core::make_ndarray({1.1, 2.2, 3.3, 4.4});
-    fmt::print("{}\n", ndarray.to_string());
+    std::cout<<ndarray.to_string()<<std::endl;
 
 /*
     core::make_ndarray needs to pass shape as a parameter, shape is of type vector<int64_t>,
@@ -43,23 +43,23 @@ void tutorial_5_ndarray() {
     std::vector<int64_t> shape = {2, 3};
     core::NDArrayRef<double> ndarray_shape = core::make_ndarray<double>(2, shape);
     core::NDArrayRef<double> ndarray_shape2 = core::make_ndarray<double>(2, {2, 3});
-    fmt::print("{}\n", ndarray_shape.to_string());
+    std::cout<<ndarray_shape.to_string()<<std::endl;
     core::NDArrayRef<double> ones = core::ones<double>(shape);
     core::NDArrayRef<double> zeros = core::zeros<double>(shape);
 
 //  core::arange currently supports only integers
     core::NDArrayRef<int> ndarray_arange = core::arange<int>(0, 16, 2);
-    fmt::print("{}\n", ndarray_arange.to_string());
+    std::cout<<ndarray_arange.to_string()<<std::endl;
 
 
     auto a = core::arange<int>(0, 16);
     auto b = a.reshape({4, 4});
-    fmt::print("{}\n", b.to_string());
+    std::cout<<b.to_string()<<std::endl;
 
 //  permute gets a new array from the index array, and the type of the index array should be vector<int64_t>.
     std::vector<int64_t> permute = {3, 2, 1, 10};
     auto c = a.permute(permute);
-    fmt::print("{}\n", c.to_string());
+    std::cout<<c.to_string()<<std::endl;
 
 /*
     core::Slice is a data type that contains start, stop, and step, all of which have default values.
@@ -68,15 +68,15 @@ void tutorial_5_ndarray() {
 */
     auto s1 = b.slice({ core::Slice{.stop=3}, {} });  // b[:3:, ::]
     auto s2 = b.slice({ 1, core::Slice{.step=-1} });  // b[1, ::-1]
-    fmt::print("{}\n", s1.to_string());
-    fmt::print("{}\n", s2.to_string());
+    std::cout<<s1.to_string()<<std::endl;
+    std::cout<<s2.to_string()<<std::endl;
 
 /*
     Iterator supports pos and index. pos gets the position relative to the data store and index 
     gets the position interpreted by strides and offset.
 */
     auto slice_b = b.slice({ core::Slice{.step = 2}, core::Slice{.step = 2}});
-    fmt::print("{}\n", slice_b.to_string());
+    std::cout<<slice_b.to_string()<<std::endl;
     auto iter = slice_b.begin();
     ++iter;
     assert(*iter == 2);
