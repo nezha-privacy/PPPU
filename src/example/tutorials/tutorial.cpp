451ffd230f1ad39dbc3e08c1a9a731affd57a16d
zcbbb266 <zcb175639@gmail.com>
2024-10-09 09:40:13 +0800

    Update tutorial.cpp

--- src/example/tutorials/tutorial.cpp
+++ src/example/tutorials/tutorial.cpp
@@ -6,6 +6,7 @@
 #include "tutorial_4_network.cpp"
 #include "tutorial_5_ndarray.cpp"
 #include "tutorial_6_visibility.cpp"
+#include "tutorial_7_smap.cpp"
 
 int main(int argc, char* argv[]) {
     int pid = std::stoi(argv[1]);
@@ -15,10 +16,11 @@ int main(int argc, char* argv[]) {
     /// @example The first terminal  : ./tutorial 0 2
     ///          The Second terminal : ./tutorial 1 2
     //tutorial::tutorial_1_prepare(pid, num_parties);
-    tutorial::tutorial_2_context(pid, num_parties);
+    //tutorial::tutorial_2_context(pid, num_parties);
     //tutorial::tutorial_3_mpc(pid, num_parties);
     //tutorial::tutorial_4_network(pid, num_parties);
     //tutorial::tutorial_5_ndarray();
     //tutorial::tutorial_6_visibility(pid, num_parties);
+    tutorial::tutorial_7_smap(pid, num_parties);
     return 0;
 }
